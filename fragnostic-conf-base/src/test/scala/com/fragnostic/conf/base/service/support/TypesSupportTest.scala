83194058aca9a13840c95a4078c48a098cac37ca
Fernando Brûlé <fernandobrule@gmail.com>
2022-01-27 12:21:18 -0300

    change api signature

--- fragnostic-conf-base/src/test/scala/com/fragnostic/conf/base/service/support/TypesSupportTest.scala
+++ fragnostic-conf-base/src/test/scala/com/fragnostic/conf/base/service/support/TypesSupportTest.scala
@@ -7,45 +7,48 @@ class TypesSupportTest extends AnyFunSpec with TypesSupport {
   describe("Types Support Test") {
 
     it("Can Get Booleam") {
-      assertResult(toType(Some("true"), (a: String) => a.toBoolean) fold (
+      assertResult(toType("true", (a: String) => a.toBoolean) fold (
         error => false,
-        opt => opt exists (myBoolean => myBoolean) //
-      ))(true)
+        myBoolean => myBoolean //
+      ) //
+      )(true)
     }
 
     it("Can Get Long") {
-      assertResult(toType(Some("123"), (a: String) => a.toLong) fold (
+      assertResult(toType("123", (a: String) => a.toLong) fold (
         error => 456L,
-        opt => opt map (myLong => myLong) getOrElse 789L //
-      ))(123L)
+        myLong => myLong //
+      ) //
+      )(123L)
     }
 
     it("Can Get Short") {
-      assertResult(toType(Some("123"), (a: String) => a.toShort) fold (
+      assertResult(toType("123", (a: String) => a.toShort) fold (
         error => 456,
-        opt => opt map (myShort => myShort) getOrElse 789 //
-      ))(123)
+        myShort => myShort //
+      ) //
+      )(123)
     }
 
     it("Can Get Int") {
-      assertResult(toType(Some("123"), (a: String) => a.toInt) fold (
+      assertResult(toType("123", (a: String) => a.toInt) fold (
         error => 456,
-        opt => opt map (myInt => myInt) getOrElse 789 //
+        myInt => myInt //
       ))(123)
     }
 
     it("Can Get Int Alternative") {
-      assertResult(toType(None, (a: String) => a.toInt) fold (
-        error => 456,
-        opt => opt map (myInt => myInt) getOrElse 789 //
-      ))(789)
+      assertResult(toType("", (a: String) => a.toInt) fold (
+        error => error,
+        myInt => myInt //
+      ))("""For input string: """"")
     }
 
     it("Can Get Int And Handle Error") {
-      assertResult(toType(Some("qwerty"), (a: String) => a.toInt) fold (
-        error => 456,
-        opt => opt map (myInt => myInt) getOrElse 789 //
-      ))(456)
+      assertResult(toType("qwerty", (a: String) => a.toInt) fold (
+        error => error,
+        myInt => myInt //
+      ))("""For input string: "qwerty"""")
     }
 
   }
